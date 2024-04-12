249d6532ba02e7353958285fb426a89cc95b74a5
haresahani <111163929+haresahani@users.noreply.github.com>
2024-04-12 12:41:47 +0530

    Add files via upload

---
+++ Tries/SearchInTrie.java
@@ -0,0 +1,31 @@
+
+public class SearchInTrie {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static boolean search(String key) { // O(L)
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow == true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "the", "a", "there", "their", "any", "thee" };
+    }
+}
